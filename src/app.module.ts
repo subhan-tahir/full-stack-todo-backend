8c3395fe818de938237748a287b2a59763d7e844
subhan-tahir <tsubhan822gmail.com>
2025-08-06 17:30:23 +0500

    log port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { TodoModule } from './todo/todo.module';
+  import { Module } from '@nestjs/common';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
+  import { MongooseModule } from '@nestjs/mongoose';
+  import { ConfigModule } from '@nestjs/config';
+  import { TodoModule } from './todo/todo.module';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URL as string, {
-      connectionFactory: (connection) => {
-        connection.on('connected', () => {
-          console.log('✅ MongoDB connected');
-        });
-        connection.on('error', (err : any) => {
-          console.error('❌ MongoDB error:', err);
-        });
-        return connection;
-      },
-    }),
-    TodoModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {
-  constructor() {
-    console.log('MongoDB_URL', process.env.MONGODB_URL);
+  @Module({
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      MongooseModule.forRoot(process.env.MONGODB_URL as string, {
+        connectionFactory: (connection) => {
+          connection.on('connected', () => {
+            console.log('✅ MongoDB connected');
+          });
+          connection.on('error', (err : any) => {
+            console.error('❌ MongoDB error:', err);
+          });
+          return connection;
+        },
+      }),
+      TodoModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {
+    constructor() {
+    
+    }
   }
-}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { whitelist } from 'validator';
 
 async function bootstrap() {
- console.log('🚀 MONGODB_URL:', process.env.MONGODB_URL);
+  console.log('🚀 MONGODB_URL:', process.env.MONGODB_URL);
+console.log('PORT',process.env.PORT);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true , transform: true ,  forbidNonWhitelisted: true,}));
-  app.enableCors();//allow any origin from frontend
-  await app.listen(process.env.PORT ?? 4000);
+
+  app.useGlobalPipes(new ValidationPipe({ 
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  app.enableCors(); // allow any origin
+
+  const port = process.env.PORT || 4000;
+
+  // ✅ this is crucial for Railway to access your app
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
